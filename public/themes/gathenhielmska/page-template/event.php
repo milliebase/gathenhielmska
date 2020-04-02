18c48a270709a2565342b64019a4d310e733a051
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-04-02 10:16:29 +0200

    added posts in event

--- public/themes/gathenhielmska/page-template/event.php
+++ public/themes/gathenhielmska/page-template/event.php
@@ -1,6 +1,6 @@
 <?php /* Template name: Event */
 get_header(); ?>
 
-<?php echo(do_shortcode('[events per_page="6" orderby="event_start_date" location="Trädgården"]')); ?>
+<?php echo(do_shortcode('[events per_page="6" orderby="event_start_date"]')); ?>
 
 <?php get_footer(); ?>
