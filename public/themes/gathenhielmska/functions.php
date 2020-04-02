1d294074a4a3e7e599639df41e8fdee16ea580e1
milliebase <betsyalva@milliebase.se>
2020-04-03 01:26:10 +0200

    added register menu, menu styling

--- public/themes/gathenhielmska/functions.php
+++ public/themes/gathenhielmska/functions.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 add_action('after_setup_theme', function () {
+    add_theme_support('menus');
     add_theme_support('soil-clean-up');
     add_theme_support('soil-disable-asset-versioning');
     add_theme_support('soil-disable-trackbacks');
@@ -11,8 +12,23 @@ add_action('after_setup_theme', function () {
     add_theme_support('soil-relative-urls');
 });
 
+//Enqueue styles and scripts
+add_action(
+    'wp_enqueue_scripts',
+    function () {
+        wp_enqueue_style('app.css', get_stylesheet_directory_uri() . '/assets/styles/app.css');
+        wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,800,800i|Roboto+Condensed:300,300i,400,400i,700,700i', false);
+        wp_enqueue_script('app.js', get_template_directory_uri() . '/assets/scripts/app.js');
+        wp_enqueue_script('menu.js', get_template_directory_uri() . '/assets/scripts/menu.js');
+    }
+);
+
+add_action('admin_menu', 'archive_admin_menu');
+
 add_filter('show_admin_bar', '__return_false');
 add_filter('enter_title_here', 'wp_change_title_text');
+
+register_nav_menu('header_menu', __('Header menu'));
 
 //Add functionality
 require get_template_directory() . '/plate.php';
@@ -34,18 +50,6 @@ require get_template_directory() . '/fields/archive-video.php';
 //Register taxonomies
 require get_template_directory() . '/taxonomies/archive-category-image.php';
 require get_template_directory() . '/taxonomies/archive-category-video.php';
-
-add_action('admin_menu', 'archive_admin_menu');
-
-//Enqueue styles and scripts
-add_action(
-    'wp_enqueue_scripts',
-    function () {
-        wp_enqueue_style('app.css', get_stylesheet_directory_uri() . '/assets/styles/app.css');
-        wp_enqueue_script('app.js', get_template_directory_uri() . '/assets/scripts/app.js');
-        wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,800,800i|Roboto+Condensed:300,300i,400,400i,700,700i', false);
-    }
-);
 
 
 //Functions
--- public/themes/gathenhielmska/header.php
+++ public/themes/gathenhielmska/header.php
@@ -11,14 +11,53 @@
 
 <body <?php body_class(); ?>>
 
-    <nav>
-        <ul>
-            <?php foreach (get_pages(['sort_column' => 'menu_order']) as $page) : ?>
-                <li>
-                    <a class="nav-link" href="<?php echo get_permalink($page); ?>">
-                        <?php echo $page->post_title; ?>
+    <div class="navbar">
+        <div class="navbar__panel">
+            <div class="navbar__buttons">
+                <a href="<?php echo home_url(); ?>" class="navbar__logo navbar__logo--hidden">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/gathenhielmska.svg" alt="Gathenhielmska logo" />
+                </a>
+
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/menu.svg" alt="Hamburger menu" />
+            </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="navbar__panel__wave navbar__panel__wave--hidden">
+                <path fill="#18595B" fill-opacity="1" d="M0,256L48,240C96,224,192,192,288,160C384,128,480,96,576,122.7C672,149,768,235,864,240C960,245,1056,171,1152,170.7C1248,171,1344,245,1392,282.7L1440,320L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+            </svg>
+        </div>
+
+        <div class="navbar__overlay hidden">
+            <div class="navbar__overlay__panel">
+                <div class="language">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/language.svg" alt="Choose language icon">
+                    <small>SV</small>
+                </div>
+
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/exit.svg" alt="Exit menu button">
+            </div>
+
+            <?php wp_nav_menu([
+                'menu_class' => 'menu',
+                'menu_id' => '',
+                'container' => '',
+                'theme_location' => 'header_menu'
+            ]) ?>
+
+            <div class="information">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/gathenhielmska_gradient.svg" alt="Gathenhielmska logo with gradient" class="information__logo">
+
+                <div class="information__ctu">
+                    <p>Login</p>
+
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/facebook.svg" alt="Gathenhielmska Facebook link">
                     </a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </nav>
+
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/instagram.svg" alt="Gathenhielmska Instagram link">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/navbar-->
