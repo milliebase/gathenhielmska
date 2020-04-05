a4a34b43326cd88fb5b5f3287273a06aea684bc8
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-04-05 11:52:39 +0200

    Added functionality to event pages

--- public/themes/gathenhielmska/page-template/event.php
+++ public/themes/gathenhielmska/page-template/event.php
@@ -2,11 +2,8 @@
 
 <?php get_header(); ?>
 
-<div class="container">
-
-    <h2 class="container__title"><?php the_title(); ?></h2>
+    <h2><?php the_title(); ?></h2>
 
     <?php echo (do_shortcode('[events per_page="6" orderby="event_start_date" ]')); ?>
 
-</div>
 <?php get_footer(); ?>
