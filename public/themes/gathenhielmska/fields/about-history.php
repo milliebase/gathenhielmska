13d00daf56d430adee7f98c60953b859e101a6d1
Marcusiscode <marcusaugustssonn@gmail.com>
2020-04-06 14:27:11 +0200

    hero function

--- public/themes/gathenhielmska/fields/about-history.php
+++ public/themes/gathenhielmska/fields/about-history.php
@@ -2,41 +2,18 @@
 
 declare(strict_types=1);
 
+
 if (function_exists('acf_add_local_field_group')) {
 
     acf_add_local_field_group(
         [
-            'key' => 'gruop_about/about-history_hero',
+            'key' => 'group_about/about_history_hero',
             'title' => 'Hero content',
             'fields' => [
+                fieldshero('about/about_history')['img'],
+                fieldshero('about/about_history')['header'],
+                fieldshero('about/about_history')['text'],
                 [
-                    'key' => 'about/about-history_hero_image',
-                    'label' => 'Hero image',
-                    'name' => 'Hero image',
-                    'type' => 'image',
-                    'instructions' => '',
-                    'required' => 1,
-                    'conditional_logic' => 0,
-                    'default_value' => '',
-                    'max_size' => '2MB',
-                    'instructions' => 'Add a hero for your about-history page.',
-                ],
-                [
-                    'key' => 'about/about-history_hero_heading',
-                    'label' => 'Heading for hero',
-                    'name' => 'headingforhero',
-                    'type' => 'text',
-                    'required' => 0,
-                    'placeholder' => '',
-                ],
-                [
-                    'key' => 'about/about-history_hero_text',
-                    'label' => 'Text',
-                    'name' => 'text',
-                    'type' => 'textarea',
-                    'required' => 0,
-                    'placeholder' => 'Add text for your about-history page.',
-                ],[
                     'key' => 'field_about-history-year',
                     'label' => 'The house years of history',
                     'name' => 'The years of history',
