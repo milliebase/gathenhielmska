935541cab8a02cac7b4e0c8394e08a81772640c3
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-04-09 17:45:01 +0200

    Add search

---
+++ public/themes/gathenhielmska/searchform.php
@@ -0,0 +1,4 @@
+<form role="search" method="get" class="search-form" action="/">
+	<input type="search" class="search-field" value="<?php the_search_query(); ?>" name="s"/>
+	<button type="submit" class="search-submit">Sök</button>
+</form>
