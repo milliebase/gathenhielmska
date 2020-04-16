a60e8243d07206dbd9dd0c6b84b9b981fe4da09d
milliebase <betsyalva@milliebase.se>
2020-04-16 16:52:29 +0200

    media queries for visit

--- public/themes/gathenhielmska/page-template/visit.php
+++ public/themes/gathenhielmska/page-template/visit.php
@@ -11,21 +11,21 @@ get_header();
     </div>
 
     <div class="visit__adress">
-        <h2 class="page__heading"><?php echo get_field('location_heading'); ?></h2>
-        <p><?php echo get_field('location_adress'); ?></p>
-        <p><?php echo get_field('location_public_transport'); ?></p>
-    </div>
+        <div class="visit__adress__text">
+            <h2 class="page__heading"><?php echo get_field('location_heading'); ?></h2>
+            <p><?php echo get_field('location_adress'); ?></p>
+            <p><?php echo get_field('location_public_transport'); ?></p>
+        </div>
 
-    <div class="acf-map">
-        <div class="marker" data-lat="57.698785" data-lng="11.934091"></div>
+        <div class="acf-map">
+            <div class="marker" data-lat="57.698785" data-lng="11.934091"></div>
+        </div>
     </div>
 
     <div class="visit__parking">
         <h2 class="page__heading"><?php echo get_field('parking_heading'); ?></h2>
         <p><?php echo get_field('parking_text'); ?></p>
     </div>
-
-
 </section>
 
 
